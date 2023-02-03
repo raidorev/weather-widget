b17da59925ab4a77a7d602f09592b1f9aec64c82
Alexander Titov <fox@raidorev.tech>
2023-02-03 20:33:06 +0600

    feat(locations): add locations composable

---
+++ src/composables/locations.spec.ts
@@ -0,0 +1,50 @@
+import type { Location } from '@/types/weather'
+import { describe, it, expect } from 'vitest'
+import { useLocations } from './locations'
+
+describe.concurrent('locations', () => {
+  it('should have 3 example locations by default', () => {
+    const { locations } = useLocations()
+    expect(locations.value).toHaveLength(3)
+  })
+
+  it('should add location', () => {
+    const { locations, addLocation, removeLocation } = useLocations()
+    addLocation({
+      name: 'Paris',
+      country: 'FR',
+      latitude: 48.8566,
+      longitude: 2.3522,
+    })
+    expect(locations.value).toHaveLength(4)
+
+    removeLocation(3)
+  })
+
+  it('should remove location', () => {
+    const paris: Location = {
+      name: 'Paris',
+      country: 'FR',
+      latitude: 48.8566,
+      longitude: 2.3522,
+    }
+    const paris2: Location = {
+      name: 'Paris 2',
+      country: 'FR',
+      latitude: 48.8566,
+      longitude: 2.3522,
+    }
+
+    const { locations, addLocation, removeLocation } = useLocations()
+
+    addLocation(paris)
+    addLocation(paris2)
+
+    removeLocation(4)
+
+    expect(locations.value).toHaveLength(4)
+    expect(locations.value[3]).toMatchObject(paris)
+
+    removeLocation(4)
+  })
+})
---
+++ src/composables/locations.ts
@@ -0,0 +1,19 @@
+import type { Location } from '@/types/weather'
+import { ref } from 'vue'
+
+const locations = ref<Location[]>([
+  { name: 'London', country: 'UK', latitude: 51.5074, longitude: 0.1278 },
+  { name: 'New York', country: 'US', latitude: 40.7128, longitude: -74.006 },
+  { name: 'Tokyo', country: 'JP', latitude: 35.6895, longitude: 139.6917 },
+])
+
+export const useLocations = () => {
+  const addLocation = (location: Location) => {
+    locations.value.push(location)
+  }
+  const removeLocation = (index: number) => {
+    locations.value.splice(index, 1)
+  }
+
+  return { locations, addLocation, removeLocation }
+}
