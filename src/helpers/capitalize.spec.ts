0baffd21413d96cb40799f1f0440df105c0b5729
Alexander Titov <fox@raidorev.tech>
2023-02-03 20:19:06 +0600

    feat(capitalize): add capitalize helper

---
+++ src/helpers/capitalize.spec.ts
@@ -0,0 +1,16 @@
+import { describe, expect, it } from 'vitest'
+import { capitalize } from './capitalize'
+
+describe.concurrent('capitalize', () => {
+  describe('when value is empty', () => {
+    it('should return empty string', () => {
+      expect(capitalize('')).toBe('')
+    })
+  })
+
+  describe('when value is not empty', () => {
+    it('should return capitalized string', () => {
+      expect(capitalize('hello')).toBe('Hello')
+    })
+  })
+})
