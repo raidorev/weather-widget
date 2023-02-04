66596527e9eafcc127712d1d157e2077bd4bef4b
Alexander Titov <fox@raidorev.tech>
2023-02-05 01:13:54 +0600

    test(weather-screen): provide locations injection

--- src/components/weather-screen.spec.ts
+++ src/components/weather-screen.spec.ts
@@ -1,17 +1,31 @@
-import { mount } from '@vue/test-utils'
 import { describe, expect, it } from 'vitest'
+import { mount } from '@vue/test-utils'
+import { locationsKey } from '@/injection-keys'
+import { locationsInjectionMock } from '@/tests/mocks/locations-injection'
 import WeatherScreen from './weather-screen.vue'
 
 describe('weather-screen', () => {
   it('should have settings button with icon', () => {
-    const wrapper = mount(WeatherScreen)
+    const wrapper = mount(WeatherScreen, {
+      global: {
+        provide: {
+          [locationsKey as symbol]: locationsInjectionMock,
+        },
+      },
+    })
     const button = wrapper.find('button')
     expect(button.exists()).toBeTruthy()
     expect(button.find('svg').exists()).toBeTruthy()
   })
 
   it('should emit "open-settings" event when close icon is clicked', () => {
-    const wrapper = mount(WeatherScreen)
+    const wrapper = mount(WeatherScreen, {
+      global: {
+        provide: {
+          [locationsKey as symbol]: locationsInjectionMock,
+        },
+      },
+    })
     wrapper.find('button').trigger('click')
     expect(wrapper.emitted('open-settings')).toHaveLength(1)
   })
