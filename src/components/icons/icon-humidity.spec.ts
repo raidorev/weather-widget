97dc144b391aa4a06e05a153fbf072529ef6c178
Alexander Titov <fox@raidorev.tech>
2023-02-04 20:13:16 +0600

    feat(icons): add some icons

---
+++ src/components/icons/icon-humidity.spec.ts
@@ -0,0 +1,7 @@
+import { describe } from 'vitest'
+import { testIconComponent } from '@/tests/test-icon-component'
+import IconHumidity from './icon-humidity.vue'
+
+describe('icon-humidity', () => {
+  testIconComponent(IconHumidity, 'Humidity')
+})
---
+++ src/components/icons/icon-wind.spec.ts
@@ -0,0 +1,7 @@
+import { describe } from 'vitest'
+import { testIconComponent } from '@/tests/test-icon-component'
+import IconWind from './icon-wind.vue'
+
+describe('icon-wind', () => {
+  testIconComponent(IconWind, 'Wind')
+})
