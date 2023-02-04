ebe4fc381acb101b9c88d8278d880c0a10053010
Alexander Titov <fox@raidorev.tech>
2023-02-04 20:37:34 +0600

    feat(weahter-info): add weather-info component

---
+++ src/components/weather-info.spec.ts
@@ -0,0 +1,56 @@
+import { capitalize } from '@/helpers/capitalize'
+import type { Location, Weather } from '@/types/weather'
+import { mount } from '@vue/test-utils'
+import { describe, expect, it } from 'vitest'
+import LocationWeather from './weather-info.vue'
+
+describe('location-weather', () => {
+  const location = {
+    name: 'London',
+    country: 'UK',
+    latitude: 51.5074,
+    longitude: 0.1278,
+  } as Location
+  const weather = {
+    tempeture: 20.5,
+    feelsLike: 18.3,
+    humidity: 67.4,
+    dewPoint: 4.5,
+    pressure: 966,
+    windSpeed: 4,
+    windDirection: 'N',
+    visibility: 10000,
+    description: 'overcast clouds',
+    icon: '',
+  } as Weather
+
+  it('should have city and county as a title', () => {
+    const wrapper = mount(LocationWeather, {
+      props: { location, weather },
+    })
+
+    expect(wrapper.find('h4').text()).toBe('London, UK')
+  })
+
+  it('should have all needed info', () => {
+    const wrapper = mount(LocationWeather, {
+      props: { location, weather },
+    })
+
+    const text = wrapper.text()
+    const infos = [
+      `${weather.tempeture}°C`,
+      `Feels like ${weather.feelsLike}°C`,
+      capitalize(weather.description),
+      `${weather.windSpeed}m/s ${weather.windDirection}`,
+      `${weather.pressure}hPa`,
+      `Humidity: ${weather.humidity}%`,
+      `Dew point: ${weather.dewPoint}°C`,
+      `Visibility: 10.0km`,
+    ]
+
+    for (const info of infos) {
+      expect(text).toContain(info)
+    }
+  })
+})
---
+++ src/components/weather-info.vue
@@ -0,0 +1,65 @@
+<script setup lang="ts">
+import IconDewPoint from '@/components/icons/icon-dew-point.vue'
+import IconHumidity from '@/components/icons/icon-humidity.vue'
+import IconPressure from '@/components/icons/icon-pressure.vue'
+import IconVisibility from '@/components/icons/icon-visibility.vue'
+import IconWind from '@/components/icons/icon-wind.vue'
+import { capitalize } from '@/helpers/capitalize'
+import type { Location, Weather } from '@/types/weather'
+
+defineProps<{ location: Location; weather: Weather }>()
+
+/**
+ * Display distance in meters or kilometers
+ */
+const displayDistance = (distance: number) => {
+  if (distance < 1000) {
+    return `${distance}m`
+  }
+
+  return `${(distance / 1000).toFixed(1)}km`
+}
+</script>
+
+<template>
+  <div class="flex items-center justify-between">
+    <h4 class="text-xl font-bold">
+      {{ location.name }}, {{ location.country }}
+    </h4>
+  </div>
+  <div class="my-6 flex items-center">
+    <img :src="weather.icon" alt="Weather condition icon" />
+    <span class="text-4xl font-bold">{{ weather.tempeture }}°C</span>
+  </div>
+
+  <div class="mb-2 flex items-center">
+    Feels like {{ weather.feelsLike }}°C. {{ capitalize(weather.description) }}.
+  </div>
+
+  <div class="mb-2 flex items-center">
+    <div class="flex flex-1 items-center">
+      <icon-wind class="mr-1 h-4 w-4" />
+      <span>{{ weather.windSpeed }}m/s {{ weather.windDirection }}</span>
+    </div>
+    <div class="flex flex-1 items-center">
+      <icon-pressure class="mr-1 h-4 w-4" />
+      <span>{{ weather.pressure }}hPa</span>
+    </div>
+  </div>
+
+  <div class="mb-2 flex items-center">
+    <div class="flex flex-1 items-center">
+      <icon-humidity class="mr-1 h-4 w-4" />
+      <span>Humidity: {{ weather.humidity }}%</span>
+    </div>
+    <div class="flex flex-1 items-center">
+      <icon-dew-point class="mr-1 h-4 w-4" />
+      <span>Dew point: {{ weather.dewPoint }}°C</span>
+    </div>
+  </div>
+
+  <div class="flex items-center">
+    <icon-visibility class="mr-1 h-4 w-4" />
+    <span>Visibility: {{ displayDistance(weather.visibility) }}</span>
+  </div>
+</template>
