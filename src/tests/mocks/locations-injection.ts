8a90bc9b89d8f8c3c03c1882dd70c1d686e11067
Alexander Titov <fox@raidorev.tech>
2023-02-05 01:13:23 +0600

    test: mock locations injection

---
+++ src/tests/mocks/locations-injection.ts
@@ -0,0 +1,13 @@
+import { ref } from 'vue'
+import type { LocationsInjection } from '@/injection-keys'
+import type { Location } from '@/types/weather'
+
+export const locationsInjectionMock = {
+  locations: ref<Location[]>([]),
+  addLocation(location: Location) {
+    this.locations.value.push(location)
+  },
+  removeLocation(index: number) {
+    this.locations.value.splice(index, 1)
+  },
+} as LocationsInjection
