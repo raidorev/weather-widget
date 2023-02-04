ab7dbca659460e9e3bfc283727b0ec1064c83c92
Alexander Titov <fox@raidorev.tech>
2023-02-05 01:14:20 +0600

    feat(settings-screen): implement settings screen)

--- src/components/settings-screen.spec.ts
+++ src/components/settings-screen.spec.ts
@@ -1,23 +1,85 @@
-import { mount } from '@vue/test-utils'
 import { describe, expect, it } from 'vitest'
+import { mount } from '@vue/test-utils'
+import { locationsKey } from '@/injection-keys'
+import { locationsInjectionMock } from '@/tests/mocks/locations-injection'
 import SettingsScreen from './settings-screen.vue'
+import type { Location } from '@/types/weather'
+import SearchBar from './search-bar.vue'
 
 describe('settings-screen', () => {
   it('should have h4 with "Settings" text', () => {
-    const wrapper = mount(SettingsScreen)
+    const wrapper = mount(SettingsScreen, {
+      global: {
+        provide: {
+          [locationsKey as symbol]: locationsInjectionMock,
+        },
+      },
+    })
     expect(wrapper.find('h4').text()).toBe('Settings')
   })
 
   it('should have close button with icon', () => {
-    const wrapper = mount(SettingsScreen)
+    const wrapper = mount(SettingsScreen, {
+      global: {
+        provide: {
+          [locationsKey as symbol]: locationsInjectionMock,
+        },
+      },
+    })
     const button = wrapper.find('button')
     expect(button.exists()).toBeTruthy()
     expect(button.find('svg').exists()).toBeTruthy()
   })
 
   it('should emit "close" event when close icon is clicked', () => {
-    const wrapper = mount(SettingsScreen)
+    const wrapper = mount(SettingsScreen, {
+      global: {
+        provide: {
+          [locationsKey as symbol]: locationsInjectionMock,
+        },
+      },
+    })
     wrapper.find('button').trigger('click')
     expect(wrapper.emitted('close')).toHaveLength(1)
+  })
+
+  it('should have list of locations', () => {
+    const locations: Location[] = [
+      {
+        name: 'Location 1',
+        country: 'C',
+        latitude: 0,
+        longitude: 0,
+      },
+      {
+        name: 'Location 2',
+        country: 'C',
+        latitude: 0,
+        longitude: 0,
+      },
+    ]
+
+    const wrapper = mount(SettingsScreen, {
+      global: {
+        provide: {
+          [locationsKey as symbol]: { ...locationsInjectionMock, locations },
+        },
+      },
+    })
+
+    // TODO: Mock vuedraggable and fix this test
+    // const listItems = wrapper.findAll('li')
+    // expect(listItems.length).toBe(locations.length)
+  })
+
+  it('should have search-bar component', () => {
+    const wrapper = mount(SettingsScreen, {
+      global: {
+        provide: {
+          [locationsKey as symbol]: locationsInjectionMock,
+        },
+      },
+    })
+    expect(wrapper.findComponent(SearchBar).exists()).toBeTruthy()
   })
 })
--- src/components/settings-screen.vue
+++ src/components/settings-screen.vue
@@ -1,9 +1,26 @@
 <script setup lang="ts">
+import { inject, ref } from 'vue'
+import draggable from 'vuedraggable'
+import IconBurger from '@/components/icons/icon-burger.vue'
 import IconClose from '@/components/icons/icon-close.vue'
+import SearchBar from '@/components/search-bar.vue'
+import { locationsKey, type LocationsInjection } from '@/injection-keys'
 
 const emit = defineEmits<{
   (event: 'close'): void
 }>()
+
+const drag = ref(false)
+
+const dragOptions = ref({
+  animation: 200,
+  disabled: false,
+  ghostClass: 'opacity-50',
+})
+
+const { locations, addLocation, removeLocation } = inject(
+  locationsKey,
+) as LocationsInjection
 </script>
 
 <template>
@@ -15,6 +32,44 @@ const emit = defineEmits<{
       <icon-close class="h-6 w-6" />
     </button>
 
-    <h4 class="text-xl font-bold">Settings</h4>
+    <h4 class="mb-3 text-xl font-bold">Settings</h4>
+
+    <div class="mb-3">
+      <transition-group>
+        <draggable
+          key="dragggable"
+          tag="ul"
+          item-key="name"
+          handle=".drag"
+          v-bind="dragOptions"
+          :list="locations"
+          @start="drag = true"
+          @end="drag = false"
+        >
+          <template #item="{ element }">
+            <li
+              :key="element.name"
+              class="mb-2 flex items-center bg-gray-300 p-1 last:mb-0"
+            >
+              <button class="drag mr-1 flex-none cursor-move">
+                <icon-burger class="h-6 w-6" />
+              </button>
+              <span class="flex-auto">
+                {{ element.name }}, {{ element.country }}
+              </span>
+              <button class="flex-none" @click="removeLocation(element.index)">
+                <icon-close class="h-6 w-6" />
+              </button>
+            </li>
+          </template>
+        </draggable>
+      </transition-group>
+    </div>
+
+    <search-bar @select="addLocation">
+      <template #label>
+        <span class="text-gray-400">Add location:</span>
+      </template>
+    </search-bar>
   </div>
 </template>
