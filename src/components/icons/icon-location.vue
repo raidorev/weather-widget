50169e1b7b4970945afd52ab3fc9702a6e317233
Alexander Titov <fox@raidorev.tech>
2023-02-04 23:32:57 +0600

    feat(icons): add more icons

---
+++ src/components/icons/icon-location.vue
@@ -0,0 +1,23 @@
+<template>
+  <svg
+    class="absolute left-2 top-2 h-4 w-4 stroke-current"
+    xmlns="http://www.w3.org/2000/svg"
+    fill="none"
+    viewBox="0 0 24 24"
+    stroke="currentColor"
+  >
+    <title>Location</title>
+    <path
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      stroke-width="2"
+      d="M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0z"
+    />
+    <path
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      stroke-width="2"
+      d="M15 11a3 3 0 11-6 0 3 3 0 016 0z"
+    />
+  </svg>
+</template>
---
+++ src/components/icons/icon-search.spec.ts
@@ -0,0 +1,7 @@
+import { describe } from 'vitest'
+import { testIconComponent } from '@/tests/test-icon-component'
+import IconSearch from './icon-search.vue'
+
+describe('icon-search', () => {
+  testIconComponent(IconSearch, 'Search')
+})
