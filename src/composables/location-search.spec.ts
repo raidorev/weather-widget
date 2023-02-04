a68e77878ef79e598c2e652f14383ce9c5853507
Alexander Titov <fox@raidorev.tech>
2023-02-04 23:59:25 +0600

    feat(location-search): add locaion-search composable

---
+++ src/composables/location-search.spec.ts
@@ -0,0 +1,89 @@
+import { flushPromises, mount } from '@vue/test-utils'
+import { describe, it, expect, beforeEach, vi, afterEach } from 'vitest'
+import { nextTick } from 'vue'
+import { useLocationSearch } from './location-search'
+
+describe.concurrent('location-search', () => {
+  beforeEach(() => {
+    vi.useFakeTimers()
+  })
+
+  afterEach(() => {
+    vi.useRealTimers()
+  })
+
+  it('should have empty query, empty error and no items by default', () => {
+    const wrapper = mount({
+      setup() {
+        return useLocationSearch()
+      },
+      template: '<div />',
+    })
+
+    const { error, isLoading, query, items } = wrapper.vm
+    expect(error).toBe('')
+    expect(query).toBe('')
+    expect(isLoading).toBe(false)
+    expect(items.length).toBe(0)
+  })
+
+  it('should call API for location data', async () => {
+    const wrapper = mount({
+      setup() {
+        return useLocationSearch()
+      },
+      template: '<div />',
+    })
+
+    wrapper.vm.query = 'London'
+
+    await nextTick()
+    vi.advanceTimersByTime(1000)
+    await flushPromises()
+
+    expect(wrapper.vm.isLoading).toBe(false)
+    expect(wrapper.vm.items.length).toBeGreaterThan(0)
+  })
+
+  it('should not call API for location data if query is too short', async () => {
+    const wrapper = mount({
+      setup() {
+        return useLocationSearch()
+      },
+      template: '<div />',
+    })
+
+    wrapper.vm.query = 'Lo'
+
+    await nextTick()
+    vi.advanceTimersByTime(1000)
+    await flushPromises()
+
+    expect(wrapper.vm.items.length).toBe(0)
+  })
+
+  it('should debounce API calls', async () => {
+    const fetchSpy = vi.spyOn(global, 'fetch')
+
+    const wrapper = mount({
+      setup() {
+        return useLocationSearch()
+      },
+      template: '<div />',
+    })
+
+    wrapper.vm.query = 'London'
+    await nextTick()
+    wrapper.vm.query = 'London1'
+    await nextTick()
+    wrapper.vm.query = 'London12'
+    await nextTick()
+    wrapper.vm.query = 'London123'
+    await nextTick()
+
+    vi.advanceTimersByTime(1000)
+    await flushPromises()
+
+    expect(fetchSpy).toHaveBeenCalledTimes(1)
+  })
+})
---
+++ src/composables/location-search.ts
@@ -0,0 +1,66 @@
+import { computed, onBeforeUnmount, ref, watch, watchEffect } from 'vue'
+import { debounce } from '@/helpers/debounce'
+import type { Location } from '@/types/weather'
+import { apiKey } from '@/config'
+
+/**
+ * Use OpenWeatherMap API to search for locations by some query
+ */
+export const useLocationSearch = (
+  options: { minimalQueryLength?: number } = {},
+) => {
+  const { minimalQueryLength = 3 } = options
+
+  const isLoading = ref(false)
+  const error = ref('')
+
+  const query = ref('')
+
+  const url = computed(
+    () =>
+      `https://api.openweathermap.org/geo/1.0/direct?q=${query.value}&limit=5&appid=${apiKey}`,
+  )
+
+  const items = ref<Location[]>([])
+
+  const search = debounce(async () => {
+    isLoading.value = true
+
+    try {
+      const response = await fetch(url.value)
+      const data = await response.json()
+      items.value = data.map(
+        (item: any): Location => ({
+          name: item.name,
+          country: item.country,
+          latitude: item.lat,
+          longitude: item.lon,
+        }),
+      )
+    } catch {
+      // TODO: Make error more specific
+      error.value = 'Failed to fetch search results'
+    } finally {
+      isLoading.value = false
+    }
+  }, 500)
+
+  watchEffect(() => {
+    if (!query.value) {
+      items.value = []
+      return
+    }
+
+    if (query.value.length < minimalQueryLength) {
+      return
+    }
+
+    search()
+  })
+
+  onBeforeUnmount(() => {
+    search.cancel()
+  })
+
+  return { query, items, isLoading, error }
+}
