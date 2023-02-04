b29170677d41031be3330af8cdd64486f462473f
Alexander Titov <fox@raidorev.tech>
2023-02-05 01:21:49 +0600

    fix(webfontloader): import WebFont directly

--- src/plugins/webfontloader.ts
+++ src/plugins/webfontloader.ts
@@ -1,5 +1,6 @@
+import WebFont from 'webfontloader'
+
 export const loadFonts = async () => {
-  const WebFont = await import('webfontloader')
   WebFont.load({
     google: {
       families: ['Oxygen:300,400,500,700&display=swap'],
