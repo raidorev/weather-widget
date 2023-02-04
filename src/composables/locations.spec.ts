396adc484733e43902fef0e46802a478e04a8c22
Alexander Titov <fox@raidorev.tech>
2023-02-04 19:27:34 +0600

    test(locations): add wrapper

--- src/composables/locations.spec.ts
+++ src/composables/locations.spec.ts
@@ -1,22 +1,39 @@
 import type { Location } from '@/types/weather'
+import { mount } from '@vue/test-utils'
 import { describe, it, expect } from 'vitest'
 import { useLocations } from './locations'
 
-describe.concurrent.skip('locations', () => {
+describe.concurrent('locations', () => {
   it('should have 3 example locations by default', () => {
-    const { locations } = useLocations()
-    expect(locations.value).toHaveLength(3)
+    const wrapper = mount({
+      setup() {
+        return useLocations()
+      },
+      template: '<div />',
+    })
+
+    const { locations } = wrapper.vm
+
+    expect(locations).toHaveLength(3)
   })
 
   it('should add location', () => {
-    const { locations, addLocation, removeLocation } = useLocations()
+    const wrapper = mount({
+      setup() {
+        return useLocations()
+      },
+      template: '<div />',
+    })
+
+    const { locations, addLocation, removeLocation } = wrapper.vm
+
     addLocation({
       name: 'Paris',
       country: 'FR',
       latitude: 48.8566,
       longitude: 2.3522,
     })
-    expect(locations.value).toHaveLength(4)
+    expect(locations).toHaveLength(4)
 
     removeLocation(3)
   })
@@ -35,24 +52,36 @@ describe.concurrent.skip('locations', () => {
       longitude: 2.3522,
     }
 
-    const { locations, addLocation, removeLocation } = useLocations()
+    const wrapper = mount({
+      setup() {
+        return useLocations()
+      },
+      template: '<div />',
+    })
+
+    const { locations, addLocation, removeLocation } = wrapper.vm
 
     addLocation(paris)
     addLocation(paris2)
 
     removeLocation(4)
 
-    expect(locations.value).toHaveLength(4)
-    expect(locations.value[3]).toMatchObject(paris)
+    expect(locations).toHaveLength(4)
+    expect(locations[3]).toMatchObject(paris)
 
     removeLocation(4)
   })
 
   it('should store locations in localStorage', () => {
-    const { locations } = useLocations()
+    const wrapper = mount({
+      setup() {
+        return useLocations()
+      },
+      template: '<div />',
+    })
 
-    expect(localStorage.getItem('locations')).toEqual(
-      JSON.stringify(locations.value),
-    )
+    const { locations } = wrapper.vm
+
+    expect(localStorage.getItem('locations')).toEqual(JSON.stringify(locations))
   })
 })
